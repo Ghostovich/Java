df2f4ff906a0236328f7edf319484a17e4d58f84
Ghostovich <gusarov_konstant@mail.ru>
2023-02-07 23:38:40 +0300

    s2.1

--- S/2/s1.java
+++ S/2/s1.java
@@ -8,9 +8,10 @@ public class s1 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int n = getNumberByUser(text:'Введите четное число N(>0): ');
-        char c1 = getCharByUser(text:'Введите символ с1: ');
-        char c2 = getCharByUser(text:'Введите символ с2: ');
+        int n = getNumberByUser("Введите четное число N(>0): ");
+        char c1 = getCharByUser("Введите символ с1: ");
+        char c2 = getCharByUser("Введите символ с2: ");
+        System.out.println(getString(n, c1, c2));
     }
 
     public static int getNumberByUser(String text) {
@@ -21,10 +22,18 @@ public class s1 {
     public static char getCharByUser(String text) {
         System.out.print(text);
         String c = input.next();
-        return c.charAt(index:0);
+        return c.charAt(0);
     }
 
-    public static String(){
-
+    public static String getString(int x, char a, char b) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < x; i++) {
+            if (i % 2 == 0) {
+                result.append(a);
+            } else {
+                result.append(b);
+            }
+        }
+        return result.toString();
     }
 }
---
+++ S/2/s2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+// Напишите метод, который сжимает строку. 
+// Пример: aaaabbbcdd → a4b3cd2
+
+public class s2 {
+    public static void main(String[] args) {
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("Введите данные: ");
+        String string = scaner.nextLine();
+    }
+
+    public static String compressString(String userString) {
+        StringBuilder result = new StringBuilder();
+    }
+}
