9f51c74264cb128a6889d19b2b66174f5536a489
Ghotovich <guarov_konstant@mail.ru>
2023-02-09 21:17:38 +0300

    s2.2

--- S/2/s2.java
+++ S/2/s2.java
@@ -8,9 +8,36 @@ public class s2 {
         Scanner scaner = new Scanner(System.in);
         System.out.println("Введите данные: ");
         String string = scaner.nextLine();
+        System.out.println(compressString(string));
     }
 
     public static String compressString(String userString) {
         StringBuilder result = new StringBuilder();
+        int count = 0;
+        for (int i = 0; i < userString.length()-1; i++) {
+           if (userString.charAt(i) == userString.charAt(i+1)) {
+                count++;
+           }
+           else if (userString.charAt(i)!=userString.charAt(i+1)){
+            count++;
+            result.append(userString.charAt(i));
+            if (count>1){
+            result.append(count);
+            }
+            count = 0;
+           }
+           if (i == userString.length()-2 && userString.charAt(userString.length()-2)!=userString.charAt(userString.length()-1))
+           {
+            result.append(userString.charAt(i));
+           }
+
+           else if (i == userString.length()-2 && userString.charAt(userString.length()-2)==userString.charAt(userString.length()-1))
+           {
+            count++;
+            result.append(userString.charAt(i));
+            result.append(count);
+            }
+        }
+        return result.toString();
     }
 }
