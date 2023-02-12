83d4557b449ed755511928ed8295dd75151cc8d6
Ghotovich <guarov_konstant@mail.ru>
2023-02-12 19:12:05 +0300

    h2.2

--- HW/2/h1.java
+++ HW/2/h1.java
@@ -10,27 +10,33 @@ import java.util.*;
 public class h1 {
     public static void main(String[] args) {
         String string = " {\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
-        System.out.println(getString(string));
+        System.out.println(Show(getValue(string)));
     }
 
-    public static String getString(String string) {
-        StringBuilder result = new StringBuilder();
-        String value = null;
-
-        result = $("SELECT * FROM students WHERE name = \"{value}\" AND country = \"Russia\" AND city = \"Moscow\"");
+    public static String getValue(String string) {
+        String name = null;
+        String country = null;
+        String city = null;
+        String age = null;
 
         String.split
         .substr(1, Length)
 
+
+        
+        return name;
+        return country;
+        return city;
+        return age;
+    }
+
+    public static String Show(String name, String country, String city, String age) {
+        StringBuilder result = new StringBuilder();
+
+        result = "SELECT * FROM students WHERE name ="+ name + "AND country ="+ country+" AND city ="+ city;
+
         if null
 
-        for (int i = 0; i < x; i++) {
-            if (i % 2 == 0) {
-                result.append(a);
-            } else {
-                result.append(b);
-            }
-        }
         return result.toString();
     }
 }
