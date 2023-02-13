01e41c2d137a10b2f966a619cb3d651578f082fb
Ghostovich <gusarov_konstant@mail.ru>
2023-02-13 11:38:48 +0300

    h2.4

--- HW/2/h2.java
+++ HW/2/h2.java
@@ -1,10 +1,41 @@
-import java.lang.System.Logger;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 // Реализуйте алгоритм сортировки пузырьком числового массива, 
 // результат после каждой итерации запишите в лог-файл.
 
 public class h2 {
+    private static Logger logger = Logger.getLogger(h2.class.getName());
+
     public static void main(String[] args) {
-        Logger logger = Logger;
+        Integer[] array = { 6, 9, 3, 5, 8 };
+        Integer[] newArray = sorting(array);
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(newArray));
+    }
+
+    private static Integer[] sorting(Integer[] array) {
+        boolean isSorted = false;
+        int temp;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    isSorted = false;
+
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    // logger.log(Level.INFO, Integer.toString(temp));
+                }
+            }
+        }
+
+        // for (int i = 0; i < 10; i++) {
+        // logger.log(Level.INFO, Integer.toString(i));
+        // }
+        System.out.println(Arrays.toString(array));
+        return array;
     }
 }
--- HW/2/h3.java
+++ HW/2/h3.java
@@ -10,7 +10,9 @@
 
 public class h3 {
     public static void main(String[] args) {
-        String string = " {\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+        String string = "[{\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"}," +
+                "{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"}," +
+                "{\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}]";
 
         String[] values = getValues(string);
         String result = getText(values);
@@ -24,51 +26,68 @@ public class h3 {
         String part2 = parts[1];
         String part3 = parts[2];
         String part4 = parts[3];
+        String part5 = parts[4];
+        String part6 = parts[5];
+        String part7 = parts[6];
+        String part8 = parts[7];
+        String part9 = parts[8];
 
         String[] keyValue1 = part1.split(":");
         String[] keyValue2 = part2.split(":");
         String[] keyValue3 = part3.split(":");
         String[] keyValue4 = part4.split(":");
+        String[] keyValue5 = part5.split(":");
+        String[] keyValue6 = part6.split(":");
+        String[] keyValue7 = part7.split(":");
+        String[] keyValue8 = part8.split(":");
+        String[] keyValue9 = part9.split(":");
 
-        String age = keyValue4[1];
-        age = age.substring(0, age.length() - 1);
-        String[] keyValue = new String[] { keyValue1[1], keyValue2[1], keyValue3[1], age };
+        // String age = keyValue4[1];
+        // age = age.substring(0, age.length() - 1);
+
+        String[] keyValue = new String[] { keyValue1[1], keyValue2[1], keyValue3[1],
+                keyValue4[1], keyValue5[1], keyValue6[1], keyValue7[1], keyValue8[1], keyValue9[1] };
 
         return keyValue;
     }
 
     private static String getText(String[] keyValue) {
-        String text1 = "SELECT * FROM students WHERE";
-        String text2 = " name = ";
-        String text3 = " AND";
-        String text4 = " country = ";
-        String text5 = " AND city = ";
-        String text6 = " AND age = ";
 
-        String name = keyValue[0];
-        String country = keyValue[1];
-        String city = keyValue[2];
-        String age = keyValue[3];
+        String text1 = "Студент ";
+        String text2 = " получил ";
+        String text3 = " по предмету ";
+
+        String name1 = keyValue[0].substring(1, keyValue[0].length() - 1);
+        String rating1 = keyValue[1].substring(1, keyValue[1].length() - 1);
+        String matter1 = keyValue[2].substring(1, keyValue[2].length() - 2);
+        String name2 = keyValue[3].substring(1, keyValue[3].length() - 1);
+        String rating2 = keyValue[4].substring(1, keyValue[4].length() - 1);
+        String matter2 = keyValue[5].substring(1, keyValue[5].length() - 2);
+        String name3 = keyValue[6].substring(1, keyValue[6].length() - 1);
+        String rating3 = keyValue[7].substring(1, keyValue[7].length() - 1);
+        String matter3 = keyValue[8].substring(1, keyValue[8].length() - 3);
 
         StringBuilder result = new StringBuilder();
         result.append(text1);
-        if (!name.contains("\"null\"")) {
-            result.append(text2);
-            result.append(name);
-        }
-        if (!country.contains("\"null\"")) {
-            result.append(text3);
-            result.append(text4);
-            result.append(country);
-        }
-        if (!city.contains("\"null\"")) {
-            result.append(text5);
-            result.append(city);
-        }
-        if (!age.contains("\"null\"")) {
-            result.append(text6);
-            result.append(age);
-        }
+        result.append(name1);
+        result.append(text2);
+        result.append(rating1);
+        result.append(text3);
+        result.append(matter1);
+        result.append("\n");
+        result.append(text1);
+        result.append(name2);
+        result.append(text2);
+        result.append(rating2);
+        result.append(text3);
+        result.append(matter2);
+        result.append("\n");
+        result.append(text1);
+        result.append(name3);
+        result.append(text2);
+        result.append(rating3);
+        result.append(text3);
+        result.append(matter3);
         return result.toString();
     }
 }
