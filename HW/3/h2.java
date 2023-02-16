0ff46a41058169374fafaad5e158014f701dbf30
Ghotovich <guarov_konstant@mail.ru>
2023-02-16 23:06:01 +0300

    h3

--- HW/3/h2.java
+++ HW/3/h2.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 // Пусть дан произвольный список целых чисел. Удалить из него чётные числа
 
 public class h2 {
-    
+    static Random random = new Random();
+    public static void main(String[] args) {
+        List<Integer> randomNumbers = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            randomNumbers.add(random.nextInt(0,100));
+        }
+        System.out.println(randomNumbers);
+
+        for (int i = 0; i < randomNumbers.size(); i++) {
+            if (randomNumbers.get(i)%2==0){
+                randomNumbers.remove(i);
+                i--;
+            }
+            if (randomNumbers.get(i) == null){
+                break;
+            }
+        }
+        System.out.println(randomNumbers);
+    }
 }
--- HW/3/h3.java
+++ HW/3/h3.java
@@ -1,6 +1,38 @@
 // Задан целочисленный список ArrayList. 
 // Найти минимальное, максимальное и среднее арифметичское этого списка
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class h3 {
-    
+    static Random random = new Random();
+    public static void main(String[] args) {
+        List<Integer> randomNumbers = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            randomNumbers.add(random.nextInt(0,100));
+        }
+        System.out.println(randomNumbers);
+        Integer min = randomNumbers.get(0);
+        Integer max = randomNumbers.get(0);
+        double sum = 0;
+        for (int i = 0; i < randomNumbers.size(); i++) {
+            for (int num : randomNumbers) {
+                if (num < min) {
+                    min = num;
+                }
+                if (num > max) {
+                    max = num;
+                }
+            }
+        }
+        for (int i = 0; i <randomNumbers.size(); i++) {
+            sum += randomNumbers.get(i);
+        }
+        double midSum = sum/randomNumbers.size();
+        System.out.println("Минимальное: " + min);
+        System.out.println("Максимальное: " + max);
+        System.out.println("Среднее арифметическое: " + midSum);
+    }
 }
+
